43c4f7354f4b3b8b910406a151b7f2e13e6d6bad
Rui Conti <ruiconti@gmail.com>
2020-03-13 17:10:56 -0300

    Dynamic sized slices

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,9 +1,11 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{30, 50}
 
@@ -15,3 +17,37 @@ func TestSum(t *testing.T) {
 		}
 	})
 }
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{2, 3}, []int{0, 1}, []int{1, -1, 2, -2})
+	want := []int{5, 1, 0}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t *testing.T, got, want []int) {
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("sum of valid slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{0, 9})
+		want := []int{5, 9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{2, 3, 5, 7})
+		want := []int{0, 15}
+		checkSums(t, got, want)
+	})
+}
+
+/*
+	reflect.DeepEqual is not type safe i.e it will compare any type
+	to any type
+*/
